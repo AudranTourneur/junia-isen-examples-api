f88f37285fe9af4485c5cdb1cb676a4decf69acb
AudranTourneur <git@tourneur.dev>
2024-11-23 22:48:51 +0100

    feat(terraform): experimental app creation

---
+++ terraform/app.tf
@@ -0,0 +1,21 @@
+resource "random_pet" "app_prefix" {
+  prefix = var.database_name
+  length = 1
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = "${random_pet.app_prefix.id}-example"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  os_type             = "Linux"
+  sku_name            = "P0v3"
+}
+
+resource "azurerm_linux_web_app" "example" {
+  name                = "${random_pet.app_prefix.id}-example"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_service_plan.example.location
+  service_plan_id     = azurerm_service_plan.example.id
+
+  site_config {}
+}
